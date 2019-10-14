588c17c1c480e53c70489a614a76caeb79f53705
Chante Earthwell <c.earthwell@gmail.com>
2019-10-14 17:51:16 -0500

    initial crack into exercise -- grocking it still

--- PolyDb/PolyDb/Program.cs
+++ PolyDb/PolyDb/Program.cs
@@ -2,11 +2,40 @@
 
 namespace PolyDb
 {
+    public abstract class DbConnection
+    {
+        //properties
+        //ConnectionString : string
+        //Timeout : TimeSpan
+
+        //DbConnection(ConnectionString )
+        //{
+        //    ConnectionString = " ";
+        //}
+
+        public abstract void Opening();
+    }
+
+    public class SqlConnection : DbConnection
+    {
+        public override void Opening()
+        {
+            Console.WriteLine("Opening SQL Database");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var timeTest = new TimeSpan(5, 6, 22);
+            Console.WriteLine(timeTest.ToString());
+            string stringTest = "abc";
+            Console.WriteLine(stringTest);
+
+            var db = new SqlConnection();
+            db.Opening();
         }
     }
 }
